7b168916bd0bc7fffa54c689403fd31326ec0175
Your Name <you@example.com>
2022-05-13 00:52:35 +0200

    video-3

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import  './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="nav">
+           <a href="/">amazona</a>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
